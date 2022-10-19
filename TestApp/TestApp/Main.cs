24255d2cc062e01c3e05d2ba347e1616cb62eb9f
Japser-Nuijten <81528795+Japser-Nuijten@users.noreply.github.com>
2022-10-19 14:41:02 +0200

    Basic TestApp setup

---
+++ TestApp/TestApp/Main.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TestApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            
+            Console.WriteLine("End of program");
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TestApp/TestApp/Writer.cs
@@ -0,0 +1,16 @@
+﻿using System.Formats.Asn1;
+using System.Globalization;
+using System.Reflection.PortableExecutable;
+using System.IO;
+
+
+
+
+public class Position
+{
+    public string? Joint { get; set; }
+    public int X { get; set; }
+    public int Y { get; set; }
+    public int Z { get; set; }
+
+}
